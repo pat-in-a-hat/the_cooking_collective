5126b237db260b6554574c5e579909d38a4d0711
pat-in-a-hat <wpatrickwork@gmail.com>
2022-12-05 14:17:26 -0700

    beginning navbar work

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,12 @@
+import React from 'react'
+
+export default function NavBar () {
+
+
+    return (
+        <>
+        </>
+    )
+}
+
 //clean navigation bar that utilizes React Router to load this single page app

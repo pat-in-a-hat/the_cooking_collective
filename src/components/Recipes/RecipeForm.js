d7d3a1373aca30f2da9485686ba341020c6f7633
pat-in-a-hat <wpatrickwork@gmail.com>
2022-12-14 13:10:45 -0700

    form submission behavior working

--- src/components/Recipes/RecipeForm.js
+++ src/components/Recipes/RecipeForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { formRef } from './RecipeModal'
 import {
     FormControl,
     FormLabel,
@@ -7,6 +8,7 @@ import {
     FormHelperText,
     Textarea,
     Input,
+    Button,
   } from '@chakra-ui/react'
 import {
     Form,
@@ -17,53 +19,46 @@ export default function RecipeForm ({ formRef, addRecipe}) {
 
     const [inputs, setInputs] = useState({link:'', notes:''})
 
-    console.log(inputs.link)
-    console.log(inputs.notes)
-
     //this allows us to handle multiple form values, passing them to a state
     const handleChange = (event) => {
-        const link = event.target.link
-        const notes = event.target.notes
-        setInputs({link:link, notes:notes})
+        const value = event.target.value;
+        setInputs({...inputs, [event.target.name]: value})
     }
 
     const handleSubmit = (event) => {
+        console.log('handling submit')
         event.preventDefault()
-        addRecipe(inputs)
+        addRecipe(inputs.link,inputs.notes)
     }
-
-    const isError = inputs.link === ''
 
     //add is required star later on https://chakra-ui.com/docs/components/form-control/usage
     return(
         <>
-        <Form method='post'>
-            <FormControl isInvalid={isError}>
+        <form onSubmit={handleSubmit}>
+            <FormControl>
                 <FormLabel>Recipe Link:</FormLabel>
                 <Input 
-                    type='text' 
-                    name='recipeLink' 
+                    type='url' 
+                    name='link' 
                     value={inputs.link}
                     onChange={handleChange}
                     placeholder='e.x. https://cooking.nytimes.com/recipes/1019793-one-pot-rice-and-beans'
                 />
-                {!isError ? (
-                    <FormHelperText>This should be a url.</FormHelperText>
-                ) : (
-                    <FormErrorMessage>Recipe link is required.</FormErrorMessage>
-                )}
+                <FormHelperText>This should be a url.</FormHelperText>
+            
 
                 <FormLabel>Recipe Notes:</FormLabel>
                 <Textarea 
                     type='text' 
-                    name='recipeNotes' 
+                    name='notes' 
                     value={inputs.notes} 
                     onChange={handleChange} 
                     placeholder='Enter your recipe notes here'
                 />
             </FormControl>
+            <Button colorScheme='yellow' mr={3} type='submit'>Add Recipe</Button>
             <button ref={formRef} type='submit' style={{display: 'none'}} />
-        </Form>
+        </form>
         </>
     )
 }
